062c82964c746cc7a2c14eca5d3df706b37cb5bf
mintolime <evgeniabright@yandex.ru>
2024-04-11 15:15:58 +0700

    dev: added the MainPageInfo and Main components + added link fonts

--- frontend/src/pages/Main/ui/Main.jsx
+++ frontend/src/pages/Main/ui/Main.jsx
@@ -1,7 +1,11 @@
+// import React from 'react';
+import { MainPageInfo } from '../../../widgets/MainPageInfo/MainPageInfo';
+import './Main.scss';
 export const Main = () => {
+  
   return (
-    <>
-     
-    </>
+    <main className='main'>
+      <MainPageInfo/>
+    </main>
   );
 };
