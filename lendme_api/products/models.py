f2626ceb53141295f2650c0d11cfebf6c972eaf2
Danil Y <yakushev.danil@gmail.com>
2024-04-25 23:01:28 +0300

    Added Product and Address models but these models not full. Added Admin for Users model.

--- lendme_api/products/models.py
+++ lendme_api/products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    address = models.ForeignKey(
+        "Address",
+        verbose_name="Адрес",
+        related_name="products",
+        on_delete=models.CASCADE,
+    )
+    category = ...
+    # metro = ...
+    price = models.IntegerField()
+    image = models.ImageField(upload_to="products/image_products")
+    author = models.ForeignKey(
+        "CustomUser",
+        verbose_name="Автор",
+        related_name="products",
+        on_delete=models.CASCADE,
+    )
+    deposit = ...
+    created_at = models.DateTimeField(auto_now_add=True)
+    time_period = ...
+
+
+class Address(models.Model):
+    address = ...
+    city = models.ChoicesField(
+        choices=...,
+    )
+    metro = ...
+
+    class Meta:
+        ordering = ("city",)
+        verbose_name = "Адрес"
+        verbose_name_plural = "Адреса"
+
+    def __str__(self):
+        return self.city
--- lendme_api/users/admin.py
+++ lendme_api/users/admin.py
@@ -1,1 +1,41 @@
+from django.contrib import admin
 
+from .models import CustomUser, Favorite, Review
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "name",
+        "email",
+        "phone",
+    )
+    search_fields = ("username",)
+    list_filter = ("username",)
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "product",
+    )
+    search_fields = ("user",)
+    list_filter = ("user",)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "client",
+        "seller",
+        "raiting",
+        "comment",
+        "created_at",
+    )
+    search_fields = ("client",)
+    list_filter = ("client",)
