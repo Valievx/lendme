ebb474f65572a63c4a72627771519149d5a1ad26
mintolime <evgeniabright@yandex.ru>
2024-04-29 22:41:32 +0700

    fix: fixed validation

--- frontend/src/shared/consts/validationSchemas.js
+++ frontend/src/shared/consts/validationSchemas.js
@@ -1,26 +1,30 @@
 import * as Yup from 'yup';
-import { MINLENGTHNAME, MINLENGTPASSWORD, NAMEREGEX } from './constants';
+import {MINLENGTHNAME, MINLENGTPASSWORD, NAMEREGEX, PHONEREGEX, validationMessages } from './constants';
 
 const validationSchemaAuthForms = Yup.object().shape({
 	username: Yup.string()
-		.min(MINLENGTHNAME, 'Имя должно быть больше одного символа')
-		.matches(NAMEREGEX, 'Имя должно состоять только из букв') // регулярное выражение для проверки на буквы
-		.required('Обязательное поле'),
+		.min(MINLENGTHNAME, validationMessages.name_min)
+		.matches(NAMEREGEX, validationMessages.name) 
+		.required(validationMessages.required),
+    phone: Yup.string()
+		.trim()
+    .matches(PHONEREGEX, validationMessages.phone)
+		.required(validationMessages.required),
 	email: Yup.string()
 		.trim()
-		.email('Введите верный формат email')
-		.required('Обязательное поле'),
+		.email(validationMessages.email)
+		.required(validationMessages.required),
 	password: Yup.string()
-		.min(MINLENGTPASSWORD, 'Пароль должен содержать минимум 6 символов')
-		.required('Обязательное поле'),
+		.min(MINLENGTPASSWORD, validationMessages.current_password)
+		.required(validationMessages.required),
 	confirmPass: Yup.string()
-		.oneOf([Yup.ref('password'), null], 'Пароли должны совпадать') // проверка на совпадение с паролем
-		.required('Обязательное поле'),
+		.oneOf([Yup.ref('password'), null], validationMessages.re_password) // проверка на совпадение с паролем
+		.required(validationMessages.required),
 });
 
 const validationSchemaSearch = Yup.object().shape({
 	search: Yup.string()
-		.matches(NAMEREGEX, 'Некорректный запрос')
-		.required('Обязательное поле'),
+		.matches(NAMEREGEX, validationMessages.search)
+		.required(validationMessages.required),
 });
 export { validationSchemaAuthForms, validationSchemaSearch };
