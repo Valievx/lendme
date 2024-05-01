584d5869fbbe52b30310f5a950ddfb3f34869785
Alexander Valiev <worxvaliev@gmail.com>
2024-05-01 12:10:20 +0500

    Add SmsCodeVerificationView

--- lendme_api/users/views.py
+++ lendme_api/users/views.py
@@ -6,6 +6,9 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
+
+from django.shortcuts import get_object_or_404
 
 from users.serializers import UserSerializer, PhoneSmsSerializer
 from users.models import CustomUser
@@ -27,6 +30,7 @@ class RegisterView(CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = UserSerializer
 
+
     def perform_create(self, serializer):
         """Переопределение perform_create для создания пользователя"""
         data = {
@@ -44,8 +48,10 @@ class SmsCodeCreateView(APIView):
     Требуемые данные: phone_number.
     """
 
-    @staticmethod
-    def post(request):
+    permission_classes = (AllowAny,)
+
+
+    def post(self, request):
         """Метод обрабатывает POST запрос для отправки смс кода."""
         phone_number = request.data.get('phone_number')
         serializer = PhoneSmsSerializer(data={'phone_number': phone_number})
@@ -53,10 +59,20 @@ class SmsCodeCreateView(APIView):
             try:
                 sms_code = generate_sms_code()
                 send_sms_code(phone_number, sms_code)
+
+                user = CustomUser.objects.get(phone_number=phone_number)
+                user.confirmation_code = sms_code
+                user.save()
+
                 return Response(
                     {'message': 'Смс код отправлен успешно',
-                     'Код авторизации': {sms_code}},
+                     'Никому не говорите код LendMe': sms_code},
                     status=status.HTTP_200_OK
+                )
+            except CustomUser.DoesNotExist:
+                return Response(
+                    {'message': 'Пользователь с указанным номером телефона не найден'},
+                    status=status.HTTP_404_NOT_FOUND
                 )
             except Exception as e:
                 return Response(
@@ -66,8 +82,35 @@ class SmsCodeCreateView(APIView):
         return Response(serializer.errors, status=400)
 
 
-def confirm_phone_view(request):
-    return HttpResponse("Confirm phone view placeholder")
+class SmsCodeVerificationView(APIView):
+    """
+    Проверка SMS кода
+
+    Проверка смс кода на подтверждение номера телефона.
+    Требуемые данные: phone_number, sms_code.
+    """
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        """Метод обрабатывает POST запрос для проверки смс кода."""
+        phone_number = request.data.get('phone_number')
+        sms_code = request.data.get('sms_code')
+        user = get_object_or_404(CustomUser, phone_number=phone_number)
+
+        if sms_code == user.confirmation_code:
+            user.is_phone_verified = True
+            user.save()
+            access_token = str(AccessToken.for_user(user))
+            return Response(
+                {'message': 'Номер телефона успешно подтвержден',
+                'access_token': access_token},
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {'message': 'Неправильный смс код'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
 def profile_view(request):
     return HttpResponse("Profile view placeholder")
