f8add3a844d6f24ee9283555384cbe7b8c411f3c
Alexander Valiev <worxvaliev@gmail.com>
2024-05-02 13:24:50 +0500

    working API for sending sms

--- lendme_api/users/services.py
+++ lendme_api/users/services.py
@@ -1,4 +1,10 @@
+import requests
+
 import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def generate_sms_code():
@@ -9,4 +15,26 @@ def generate_sms_code():
 def send_sms_code(phone_number, confirmation_code):
     """Отправка СМС. Иммитация."""
     message = f'Код авторизации: {confirmation_code}'
-    # print(message)
+    print(message)
+
+    # """Отправка СМС через sms.ru."""
+    # api_id = os.getenv('SMSRU_API_ID')
+    # sms_api_url = 'https://sms.ru/sms/send'
+    #
+    # params = {
+    #     'api_id': api_id,
+    #     'to': phone_number,
+    #     'msg': f'Код авторизации: {confirmation_code}',
+    #     'json': 1
+    #
+    # }
+    #
+    # response = requests.get(sms_api_url, params=params)
+    # if response.status_code == 200:
+    #     result = response.json()
+    #     if result.get('status') == 'OK':
+    #         print('Сообщение успешно отправлено')
+    #     else:
+    #         print(f'Ошибка при отправке сообщения: {result.get("status_code")}')
+    # else:
+    #     print('Ошибка при отправке запроса')
