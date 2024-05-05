5322d8afd5da62ebaa9e85e7d044a1618abcd497
mintolime <evgeniabright@yandex.ru>
2024-05-05 20:11:51 +0700

    dev: added a NotFoundPage

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,6 +2,7 @@ import './styles/App.scss';
 import { Main } from '../pages/Main';
 import { Header } from '../widgets/Header/Header';
 import { Route, Routes } from 'react-router-dom';
+import { NotFoundPage } from '../pages/NotFoundPage';
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Header />
 			<Routes>
 				<Route path="/" element={<Main />} />
+				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</>
 	);
