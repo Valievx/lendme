9ad1c831ffcc0e0fa981fa0d6b85681d818cd1b6
Mangeko <mangekos@yandex.ru>
2024-07-08 19:34:54 +0300

    fix docker and swagger

--- lendme_api/lendme_api/settings.py
+++ lendme_api/lendme_api/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
--- lendme_api/lendme_api/urls.py
+++ lendme_api/lendme_api/urls.py
@@ -21,14 +21,14 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("users.urls")),
     path("api/", include(router.urls)),
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-         "swagger/",
+         "api/swagger/",
          SpectacularSwaggerView.as_view(url_name="schema"),
          name="swagger",
      ),
     path(
-         "redoc/",
+         "api/redoc/",
          SpectacularRedocView.as_view(url_name="schema"),
          name="redoc",
      ),
